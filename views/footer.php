2f1b853a25ff800d25d106babeac50b18401b9d2
Marcus Battle <marcusbattle@Marcbook.local>
2015-03-28 03:51:24 -0400

    Resolves #14

---
+++ views/footer.php
@@ -0,0 +1,32 @@
+<div id="footer" class="hero container-fluid">
+	<div class="row">
+		<div class="col-md-3">
+			<h3>Service Times</h3>
+			<ul>
+				<li>Sunday 9AM</li>
+				<li>Tuesday 7PM</li>
+				<li>Thursday 6:30PM</li>
+			</ul>
+		</div>
+		<div class="col-md-3">
+			<h3>Address</h3>
+			<p>321 Oakview Rd.<br />High Point, NC 27265</p>
+		</div>
+		<div class="col-md-3">
+			<h3>Contact Info</h3>
+			<ul>
+				<li>info@monumentofpraise.org</li>
+				<li>(336) 887-5673</li>
+			</ul>
+		</div>
+		<div class="col-md-3">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/monument-logo-white.png" />
+			<p>&copy; <?php echo date('Y'); ?> Monument of Praise<br />All Rights Reserved.</p>
+		</div>
+	</div>
+</div>
+<div id="location-bar" class="container-fluid">
+	<div class="row text-center">
+		<p><i class="fa fa-map-marker"></i> Visit one of our other locations in <a href="http://newjc.org">Greensboro, NC</a> or <a href="http://newjc.org/locations/amazing-grace-south-africa">East London, South Africa</a></p>
+	</div>
+</div>
