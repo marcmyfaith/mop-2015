d322cd1c186ec56873d0a4c44eb26431863f9d5a
Marcus Battle <marcusbattle@Marcbook.local>
2015-03-28 05:30:21 -0400

    Updated giving page

--- page-give.php
+++ page-give.php
@@ -10,7 +10,7 @@
 	<body>
 		<div id="navigation">
 			<div class="row">
-				<h1 id="logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/monument-logo-white.png" /></a></h1>
+				<h1 id="logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/monument-logo-white.png" width="194" height="80" /></a></h1>
 				<ul id="location-menu" class="nav nav-pills pull-right">
 					<li class="hidden-xs" role="presentation"><a href="<?php echo home_url('watch'); ?>">Watch Live</a></li>
 					<li class="hidden-xs" role="presentation"><a href="<?php echo home_url('sermons'); ?>">Sermons</a></li>
@@ -20,51 +20,15 @@
 				</ul>
 			</div>
 		</div>
-		<div class="hero container-fluid">
+		<div id="hero-give" class="hero container-fluid pad-top">
 			<div class="row text-center">
-				<h2>Give</h2>
+				<h3>Give</h3>
+				<p>Monument of Praise Ministries has purchased 25 acres of land—the first step in the fulfillment of the vision to establish ministries capable of meeting the spiritual, emotional, and physical needs of a hurting community, and of establishing a legacy of praise and worship to honor the Lord, and His commitment to His people.</p>
 				<!-- <p>To pay your tithes or offering complete the form below</p> -->
-				<p>The ability to give electronically will be made available on Sunday March 28, 2015</p>
+				<p>We thank you for your desire to give and support the work we're doing in High Point, NC. The ability to give online will be made available on <strong>Easter Sunday April 5, 2015</strong>.</p>
 			</div>
 		</div>
-		<div id="footer" class="hero container-fluid">
-			<div class="row">
-				<div class="col-md-3">
-					<h3>Service Times</h3>
-					<ul>
-						<li>Sunday 9AM</li>
-						<li>Tuesday 7PM</li>
-						<li>Thursday 6:30PM</li>
-					</ul>
-				</div>
-				<div class="col-md-3">
-					<h3>Address</h3>
-					<p>321 Oakview Rd.<br />High Point, NC 27265</p>
-				</div>
-				<div class="col-md-3">
-					<h3>Contact Info</h3>
-					<ul>
-						<li>info@monumentofpraise.org</li>
-						<li>(336) 887-5673</li>
-					</ul>
-				</div>
-				<div class="col-md-3">
-					<p>&copy; <?php echo date('Y'); ?> Monument of Praise<br />All Rights Reserved.</p>
-				</div>
-			</div>
-		</div>
-		<div id="location-bar" class="container-fluid">
-			<div class="row text-center">
-				<!-- <p><i class="fa fa-map-marker"></i> Visit one of our other locations in <a href="http://newjc.org">Greensboro, NC</a> or <a href="http://newjc.org/locations/amazing-grace-south-africa">East London, South Africa</a></p> -->
-				<ul id="location-menu">
-					<li><i class="fa fa-map-marker"></i></li>
-					<li>Visit one of our other locations in</li>
-					<li><a href="http://newjc.org">Greensboro, NC</a></li>
-					<li>or</li>
-					<li><a href="http://newjc.org/locations/amazing-grace-south-africa">East London, South Africa</a></li>
-				</ul>
-			</div>
-		</div>
+		<?php get_template_part( 'views/footer' ); ?>
 		<?php wp_footer(); ?>
 	</body>
 </html>
