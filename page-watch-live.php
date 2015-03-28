f1070da1d21840aabd3160aaf6950fa0b8c4bdae
Marcus Battle <marcusbattle@Marcbook.local>
2015-03-28 04:02:41 -0400

    Resolves #13

--- page-watch-live.php
+++ page-watch-live.php
@@ -24,56 +24,22 @@ Template Name: Watch Live
 		</div>
 		<div id="hero-watch-live" class="hero container-fluid">
 			<div class="row">
-				<div id="video-holder" class="col-md-8">
-					<iframe width="720" height="437" src="http://www.ustream.tv/embed/7223948?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;">    </iframe>
-<br /><a href="http://www.ustream.tv" style="font-size: 12px; line-height: 20px; font-weight: normal; text-align: left;" target="_blank">Broadcast live streaming video on Ustream</a>
+				<div id="video-holder" class="col-md-7">
+					<iframe width="100%" height="500" src="http://www.ustream.tv/embed/7223948?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;">    </iframe>
 				</div>
-				<div class="col-md-4">
-					<a href="#" class="btn btn-default">Share The Word</a>
-				</div>
-			</div>
-		</div>
-		<div id="footer" class="hero container-fluid">
-			<div class="row">
-				<div class="col-md-3">
-					<h3>Service Times</h3>
+				<div id="content-area" class="col-md-4 col-md-offset-1">
+					<h3>Broadcasting live every Sunday, Tuesday &amp; Thursday</h3>
+					<p>We believe that our most valuable asset is the Word of God. Join us from anywhere in the world for a relevant and life changing word at one of the following times:</p>
 					<ul>
-						<li>Sunday 9AM</li>
-						<li>Tuesday 7PM</li>
-						<li>Thursday 6:30PM</li>
+						<li>Sundays @ 9AM EST</li>
+						<li>Tuesdays @ 7PM EST - Bible Study</li>
+						<li>Thursdays @ 6:30PM EST - Mid Week Service</li>
 					</ul>
-				</div>
-				<div class="col-md-3">
-					<h3>Address</h3>
-					<p>321 Oakview Rd.<br />High Point, NC 27265</p>
-				</div>
-				<div class="col-md-3">
-					<h3>Contact Info</h3>
-					<ul>
-						<li>info@monumentofpraise.org</li>
-						<li>(336) 887-5673</li>
-					</ul>
-				</div>
-				<div class="col-md-3">
-					<h3>&nbsp;</h3>
-					<p>&copy; <?php echo date('Y'); ?> Monument of Praise<br />All Rights Reserved.</p>
-					<p>Produced by BattleBranding</p>
+					<p><a class="btn btn-default" href="<?php echo home_url('sermons'); ?>">Sermons Archive</a></p>
 				</div>
 			</div>
 		</div>
-		<div id="location-bar" class="container-fluid">
-			<div class="row text-center">
-				<ul id="location-menu">
-					<li>Visit one of our other locations</li>
-					<li><i class="fa fa-map-marker"></i></li>
-					<li><a href="http://newjc.org">Greensboro, NC</a></li>
-					<li>|</li>
-					<li><a href="#">High Point, NC</a></li>
-					<li>|</li>
-					<li><a href="http://newjc.org/locations/amazing-grace-south-africa">London, South Africa</a></li>
-				</ul>
-			</div>
-		</div>
+		<?php get_template_part( 'views/footer' ); ?>
 		<?php wp_footer(); ?>
 	</body>
 </html>
