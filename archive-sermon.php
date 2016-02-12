31ad6e2b38aa4ca19da15f5553b4273bd316d4df
Marcus Battle <marcusbattle@Marcbook.local>
2016-02-12 07:14:46 -0500

    Changed text about sermon series

--- archive-sermon.php
+++ archive-sermon.php
@@ -9,8 +9,8 @@
 		<div id="hero-sermon-series" class="hero container-fluid pad-top">
 			<div class="row text-center">
 				<h5>Current Sermon Series</h5>
-				<h3>Principles</h3>
-				<p>Dr. Williams is teaching a life changing word about "Principles", laws God has ordained for maximum life benefits.</p>
+				<h3>Faith</h3>
+				<p>Dr. Williams is teaching a life changing word about "Faith".</p>
 				<p>Visit us this Sunday to experience this word for yourself.</p>
 			</div>
 		</div>
--- home.php
+++ home.php
@@ -20,8 +20,8 @@
 		<div id="hero-sermon-series" class="hero container-fluid">
 			<div class="row text-center">
 				<h5>Current Sermon Series</h5>
-				<h3>Principles</h3>
-				<p>Visit us as Dr. Williams imparts a life changing word about "Principles", laws God has ordained for maximum life benefits.</p>
+				<h3>Faith</h3>
+				<p>Visit us as Dr. Williams imparts a life changing word about "Faith", and learn how God has made you unstoppable in 2016.</p>
 				<a class="btn btn-default white" href="<?php echo home_url('sermons'); ?>">Sermons</a>
 			</div>
 		</div>
